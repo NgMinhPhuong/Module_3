3dfefaf30faccd9fafe420fb38d9b644ad85c4e2
Nguyễn Minh Phương <nguyenminhphuong813@gmail.com>
2023-12-06 21:05:47 +0700

    up

---
+++ testSQL/src/test.java
@@ -0,0 +1,17 @@
+public class test {
+    int tt = 5;
+    public test(int tt) {
+        this.pritn();
+
+
+    }
+   void pritn(){
+
+   }
+
+
+
+    public static void main(String[] args) {
+        test a = new test(4);
+    }
+}
