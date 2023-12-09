aff6ef10e3f5bc3b370e282bdde2835d142c2ce3
Nguyễn Minh Phương <nguyenminhphuong813@gmail.com>
2023-12-09 14:06:51 +0700

    up

--- ordersTOTALPRICE.sql
+++ ordersTOTALPRICE.sql
@@ -1,9 +1,8 @@
 use quanlybanhang;
 update orders
 join(
-select orderdetail.oID, sum(orderdetail.odQTY*product.pPrice) as sumVal
+select oID, sum(orderdetail.odQTY*product.pPrice) sumVal
 from orderdetail join product on orderdetail.pID = product.pID
-group by oID
-)as t
-on t.oID = orders.oID
+group by oID) as t
+on orders.oID = t.oID
 set orders.oTotalPrice = t.sumVal;
